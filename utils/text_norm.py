cd5d84a16462989f685a5ebcd3bb4c6ceeba2f6b
avarshvir <avarshvir@gmail.com>
2025-11-01 22:43:56 +0530

    Text Normalization

---
+++ utils/text_norm.py
@@ -0,0 +1,11 @@
+def lower_case(text):
+    lower_text = text.lower().split()
+    return " ".join(lower_text)
+
+def upper_case(text):
+    upper_text = text.upper().split()
+    return " ".join(upper_text)
+
+def capitalize_case(text):
+    capitalize_text = text.capitalize().split()
+    return " ".join(capitalize_text)
