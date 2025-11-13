00c0979675f109bf8e6c680835e00a8fc94cae9c
avarshvir <avarshvir@gmail.com>
2025-11-13 22:19:32 +0530

    data for NLTK and spacy

--- source_paths/nltk_stopworder.py
+++ source_paths/nltk_stopworder.py
@@ -1,6 +1,10 @@
 import nltk
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
 
-print("Downloading Stopwords....")
-nltk.download("stopwords")
+print("Downloading....")
+#nltk.download("stopwords")
+#nltk.download('punkt')
+print("---------------------------------------------------------")
+#nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
 print("Installed Successfully....")
