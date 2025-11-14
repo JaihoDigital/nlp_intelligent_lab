8b05ddec15fb4c5e16d61152d51e9b5015ff821e
avarshvir <avarshvir@gmail.com>
2025-11-14 16:15:01 +0530

    version 3.2.1

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # ===============================
 
 APP_NAME = "NLP Intelligent Lab"
-VERSION = "3.1.1"
+VERSION = "3.2.1"
 AUTHOR = "Arshvir"
 DESCRIPTION = "An interactive NLP learning platform for text preprocessing, syntax parsing, and more."
 LICENSE = "Apache License 2.0"
