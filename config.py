ea332119d6ab367b577a0cc327001367533b38f7
avarshvir <avarshvir@gmail.com>
2025-11-18 23:14:30 +0530

    version 4.3

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # ===============================
 
 APP_NAME = "NLP Intelligent Lab"
-VERSION = "4.2"
+VERSION = "4.3"
 AUTHOR = "Arshvir"
 DESCRIPTION = "An interactive NLP learning platform for text preprocessing, syntax parsing, and more."
 LICENSE = "Apache License 2.0"
